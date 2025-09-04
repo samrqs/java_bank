dde26228bbeb37ed8664f6fbe8f807ae5183c7b4
Samara Marques <samarassk@gmail.com>
2025-09-04 14:04:19 -0300

    chore: initialize Spring Boot project with Spring Initializr

---
+++ src/main/java/com/example/java_bank/JavaBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_bank/JavaBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
